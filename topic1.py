1dc8a0c68eb9a044108a7b47c5f775307b5db7a9
Rlcf22 <86522173+Rlcf22@users.noreply.github.com>
2021-07-22 18:36:00 -0300

    Update topic1.py

--- topic1.py
+++ topic1.py
@@ -1,21 +1,40 @@
-#Assign the value 5 to a, and value 6 to b. Assign the value of a + b to variable c.
 #Topic 1: Question 1
-
-
+#Assign the value 5 to a, and value 6 to b. Assign the value of a + b to variable c.
 
 a = 5
 b = 6
 c = a + b
-
 
 #Topic 1: Question 2
 #[MCQ] Which of the following will produce the right result?
 
 '2' + '5' produces '25'
 
-#Compute the area and perimeter of a circle with radius = 3
 #Topic 1: Question 3
+#Compute the area and perimeter of a circle with radius = 3
 
 pi = 3.14
 area = pi*3*3
 perimeter = 2*pi*3
+
+#Topic 1: Question 4
+# Change the type of the variable x to float
+# Change the type of variable y to integer
+
+x = 123446754336788543835697
+y = 3.14159265358979323846
+x = float(x)
+y = int(y)
+
+#Topic 1: Question 5
+# Assign foobar which gives the output shown in the last example.
+# Hint: Use the triple quote as the outermost quote
+foobar = '"No, thanks, Mom," I said, "I don\'t know how long it will take."'
+
+#Topic 1: Question 6
+
+# Assign 'HelloWorld!' to variable a
+a = 'HelloWorld!'
+
+# b contains 'HelloWorld!HelloWorld!HelloWorld!HelloWorld!HelloWorld!'
+b = 'HelloWorld!'*5
