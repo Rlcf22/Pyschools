81f5dc0bf843949299bd094a7cf1020c82879b0c
Rlcf22 <86522173+Rlcf22@users.noreply.github.com>
2021-07-22 18:32:53 -0300

    Create topic1.py

---
+++ topic1.py
@@ -0,0 +1,21 @@
+#Assign the value 5 to a, and value 6 to b. Assign the value of a + b to variable c.
+#Topic 1: Question 1
+
+
+
+a = 5
+b = 6
+c = a + b
+
+
+#Topic 1: Question 2
+#[MCQ] Which of the following will produce the right result?
+
+'2' + '5' produces '25'
+
+#Compute the area and perimeter of a circle with radius = 3
+#Topic 1: Question 3
+
+pi = 3.14
+area = pi*3*3
+perimeter = 2*pi*3
